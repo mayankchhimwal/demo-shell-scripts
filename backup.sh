cb3f4a4bbe73e820cfd03198dc9664d63f303653
Ubuntu <ubuntu@ip-172-31-43-108.eu-west-1.compute.internal>
2025-11-19 08:40:39 +0000

    add backup

---
+++ backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+<<info
+This shell script will take a perodic backups whenever we run the script
+e.g.,
+scr = /home/ubuntu/scripts
+dest = /home/ubuntu/backups
+info
+
+scr=$1
+dest=$2
+
+timestamp=$(date '+%Y-%m-%d-%H-%M')
+
+zip -r "$dest/backup-$timestamp.zip" $scr
+
+echo "Backup Completed"
+
+aws s3 sync "$dest" s3://mayank-backup-e
+
+echo "Backups are Sync in S3"
