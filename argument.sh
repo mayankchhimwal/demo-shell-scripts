fe7b15d540c6f253080437d2a60d72e8a3b9d51b
Ubuntu <ubuntu@ip-172-31-43-108.eu-west-1.compute.internal>
2025-11-19 08:36:41 +0000

    added argument

---
+++ argument.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  ./3_idiots raju farahan rancho
+
+echo "movie name $0"
+echo "first name $1"
+echo "second name $2"
+echo "third name $3"
+echo "All member $@"
+echo "Number of members $#"
